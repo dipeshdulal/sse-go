4fc31a15ff0187c30d33489e35aa2b3f5f443aab
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-07-18 14:39:31 +0545

    feat: add request buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,12 @@ let es: EventSource | null;
 
 function App() {
   const [buffer, push] = useBufferedState(10);
-  const url = "http://localhost:5000/sse";
+  const url = "http://localhost:5000";
 
   const handleStreamStart = () => {
     if (!es) {
-      es = new EventSource(url);
-      push("event source start")
+      es = new EventSource(`${url}/sse`);
+      push("event source start");
       es.onmessage = (msg) => {
         push(msg.data);
       };
@@ -22,16 +22,34 @@ function App() {
   const handleStreamStop = () => {
     if (es) {
       es.close();
-      push("close event source")
+      push("close event source");
       es = null;
     }
   };
+
+  const sendRequest = async (method: string) => {
+    await fetch(`${url}/log`, {
+      body: method != "GET" ? Math.random().toString() : null,
+      method
+    })
+  } 
 
   return (
     <div className="App">
       <h1>Event Streaming </h1>
       <button onClick={handleStreamStart}>Start Streaming</button>
       <button onClick={handleStreamStop}>Stop Streaming</button>
+      <br />
+      <br />
+      <button onClick={() => sendRequest("POST")}>POST TO /log</button>
+      <button onClick={() => sendRequest("GET")}>GET TO /log</button>
+      <button onClick={() => sendRequest("DELETE")}>
+        DELETE TO /log
+      </button>
+      <button onClick={() => sendRequest("PATCH")}>
+        PATCH TO /log
+      </button>
+      <br />
       <div className="Streams">
         {buffer.map((d, i) => (
           <pre key={i + Math.random()}>{d}</pre>
